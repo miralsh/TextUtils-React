cd7dca2af0f001caa26e18d9e06aeb03e3e03b1f
miralsh <miralharsora18@gmail.com>
2024-04-07 11:43:34 +0530

    all files

---
+++ build/precache-manifest.409644c1339949a2db504947399899b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a078222bb0d3078190ce0038291d904",
+    "url": "/TextUtils-React/index.html"
+  },
+  {
+    "revision": "937743bfd46c9df5b387",
+    "url": "/TextUtils-React/static/css/main.9752b9dc.chunk.css"
+  },
+  {
+    "revision": "8bca827d70f3d8b1f1bd",
+    "url": "/TextUtils-React/static/js/2.2edda431.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextUtils-React/static/js/2.2edda431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82594277ffdbbbbe6c96",
+    "url": "/TextUtils-React/static/js/3.f00e9902.chunk.js"
+  },
+  {
+    "revision": "937743bfd46c9df5b387",
+    "url": "/TextUtils-React/static/js/main.8f65eaf0.chunk.js"
+  },
+  {
+    "revision": "ebec0e7e4c807e308fc4",
+    "url": "/TextUtils-React/static/js/runtime-main.93eafebd.js"
+  }
+]);
